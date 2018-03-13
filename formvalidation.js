8cd9f4b8f48b51b25fe1efd36d44c159e7a0666b
Saifaligour <saifaligour41@gmail.com>
2018-03-13 22:23:49 +0530

    added

---
+++ formvalidation.js
@@ -0,0 +1,10 @@
+
+
+function phonenumber(phone){
+
+phone= phone.value.replace(/^[0-9]/,'');
+    
+ }
+
+
+// <!--<input type="text" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');" />-->
